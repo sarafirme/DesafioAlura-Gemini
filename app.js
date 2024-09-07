7680b7c994f96ad0ef1c31bc52934dacf628b82c
sarafirme <160267353+sarafirme@users.noreply.github.com>
2024-09-07 14:09:12 +0000

    Atualizando app.jse corrigindo exibicao de mensagens

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa")
-
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLocaleLowerCase();
+    let palavraEncontrada = false;
 
 
     if(!campoPesquisa){
         section.innerHTML = "<p>Nenhuma palavra foi digitada</p>"
         return
     }
-    campoPesquisa = campoPesquisa.toLowerCase()
-
 
     let resultados = "";
     let palavra = "";
 
     for (let dado of dados) {
-
         palavra = dado.palavra.toLocaleLowerCase()
         
         if(palavra.includes(campoPesquisa)){
@@ -29,12 +26,14 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank"><strong>Acesse o video demonstrativo</strong></a>
             </div>
         `;
+            palavraEncontrada = true;
         }
-        if(!resultados){
-            resultados = "<p>Desculpe, a palavra digitada ainda não está em nossa base de dados</p>"
+        if(!palavraEncontrada){
+            section.innerHTML = "<p>Desculpe, a palavra digitada não foi encontrada em nossa base de dados</p>"
+        }else{
+            section.innerHTML = resultados;
         }
     }
-    section.innerHTML = resultados;
 }
 
 
